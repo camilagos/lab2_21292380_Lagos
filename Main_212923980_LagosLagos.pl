0289990c1724e93df612ab42d1b454b9342476b6
Camila Lagos <118062775+camilagos@users.noreply.github.com>
2023-10-30 23:33:05 -0300

    Implementación y documentación
    Implementación del constructor "option" con su respectiva documentación e implementación parcial del constructor "flow".

--- Main_212923980_LagosLagos.pl
+++ Main_212923980_LagosLagos.pl
@@ -1,1 +1,12 @@
 
+%RF2-Constructor
+%option/6
+%Dom: Code (Number) X Message (String) x ChatbotCodeLink (Number) X
+%InitialFlowCodeLink (Number) X Keyword (List) X option (List)
+%Meta Primaria: option/6
+%Meta Secundaria: No aplica
+option(Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword,[Code, Message, ChatbotCodeLink, InitialFlowCodeLink, Keyword]).
+
+%RF3-falta implementar "eliminarduplicados"
+flow(Id, NameMsg, Options, [Id, NameMsg, OptionsFinal]) :-
+    eliminarduplicados(Options, OptionsFinal).
